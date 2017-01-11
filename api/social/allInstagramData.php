2ca3ebe13b618285ca27e153c8ed2ef08759199c
Andy Barefoot <andy@andybarefoot.com>
2017-01-11 10:34:17 +0000

    instagram data feed

---
+++ api/social/allInstagramData.php
@@ -0,0 +1,35 @@
+<?
+
+$COUNT = intval($_GET["count"]);
+if($COUNT<=0)$COUNT=5;
+
+$OFFSET = intval($_GET["offset"]);
+if($OFFSET<=0)$OFFSET=0;
+
+
+function cmp($a, $b){
+    return strcmp(intval($b['date']), intval($a['date']));
+}
+
+
+
+$andyInsText = file_get_contents('../instagram/andybarefoot.txt');
+$gunterInsText = file_get_contents('../instagram/gunterguntychops.txt');
+
+$andyInsData = json_decode($andyInsText, true);
+$gunterInsData = json_decode($gunterInsText, true);
+
+$andyInsNodes = $andyInsData['nodes'];
+$gunterInsNodes = $gunterInsData['nodes'];
+
+$allNodes = array_merge($andyInsNodes,$gunterInsNodes);
+usort($allNodes, "cmp");
+$nodes = array_slice($allNodes, $OFFSET, $COUNT);
+$data = array (
+	"offset" => $OFFSET,
+	"count" => $COUNT,
+);
+$data["nodes"] = $nodes;
+print json_encode($data);
+
+?>
